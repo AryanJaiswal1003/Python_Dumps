3efdb10a137191a84c291fb7f5a14a33dce8b8f8
Aryan Jaiswal <aryanjaiswal1003@yahoo.com>
2025-08-23 14:59:37 +0530

    Update main.py

--- NATO Alphabet Project/main.py
+++ NATO Alphabet Project/main.py
@@ -7,11 +7,18 @@ data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {value['letter']: value['code'] for (key, value) in data_frame.iterrows()} # iterrows() loops over the DataFrame rows as (index, row) pairs
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a Word: ").upper()
+if_true = True
 
-"""
-# Convert the user input into a list of phonetic code words. For each character in input, look up its code in dictionary.
-# If the character exists in the dictionary, add its code word to the list
-"""
-phonetic_alphabet = [nato_dict.get(item) for item in user_input if item in nato_dict]
-print(phonetic_alphabet)
+while is_true:
+  user_input = input("Enter a Word: ").upper()
+  """
+  # Convert the user input into a list of phonetic code words. For each character in input, look up its code in dictionary.
+  # If the character exists in the dictionary, add its code word to the list
+  """
+    try:
+        phonetic_alphabet = [nato_dict[letter] for letter in user_input] # Try to convert each letter to its NATO phonetic code
+        print(phonetic_alphabet)
+        is_true = False  # Stop loop after successful conversion
+
+    except KeyError:
+        print("Sorry, only Letters in the alphabet please.\n")
